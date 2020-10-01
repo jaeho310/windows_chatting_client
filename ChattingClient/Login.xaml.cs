2448a9e9bdb53f344fb89371d2434559a83fb0c4
jaeho310 <chlwogh1993@gmail.com>
2020-10-01 16:29:03 +0900

    Add LOGIN WINDOWS

---
+++ ChattingClient/Login.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChattingClient
+{
+    /// <summary>
+    /// Login.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
